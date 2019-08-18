0585fdc9098ecf3c3c14c319084b587dfdc43d05
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-08-18 00:19:40 -0500

    Create ValueRange.java

---
+++ Strings/Integer/ValueRange.java
@@ -0,0 +1,14 @@
+package classtwo;
+
+public class ValueRange {
+	public static void main(String[] args) {
+		int a = Integer.MAX_VALUE;
+		int b = Integer.MIN_VALUE;
+		System.out.println("The Max value of Integer is: "+ a);
+		System.out.println("The Min value of Integer is: "+ b);
+	}
+}
+
+Output:
+The Max value of Integer is: 2147483647
+The Min value of Integer is: -2147483648
